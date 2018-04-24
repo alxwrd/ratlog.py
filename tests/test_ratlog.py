d0cbd208c1844c46dd68de8c0fdfa84f2eeb6240
Alex Ward <alxwrd@googlemail.com>
2018-04-24 20:52:19 +0100

    add tests against spec testsuite

--- tests/test_ratlog.py
+++ tests/test_ratlog.py
@@ -1,4 +1,25 @@
+import json
+import pytest
 import ratlog
+
+
+class MockWriter(object):
+
+    def __init__(self):
+        self.value = None
+
+    def __call__(self, message):
+        self.value = message
+
+
+output = MockWriter()
+ratlog.Log.writer = output
+
+
+def get_generic_test_data():
+    with open("tests/ratlog.testsuite.json") as testsuite:
+        test_json = json.load(testsuite)
+    return [(test["log"], test["data"]) for test in test_json["generic"]]
 
 
 class TestRatlog:
@@ -7,3 +28,11 @@ class TestRatlog:
         log = ratlog.Log()
 
         assert isinstance(log, ratlog.Log)
+
+    @pytest.mark.parametrize("expected,data", get_generic_test_data())
+    def test_generic_spec_cases(self, expected, data):
+        log = ratlog.Log(*data.get("tags", []))
+
+        log(data.get("message", ""), data.get("fields"))
+
+        assert expected == output.value
