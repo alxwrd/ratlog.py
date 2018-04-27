ba1e9fba477c988701fc51c468777000d93cc9e8
Alex Ward <alxwrd@googlemail.com>
2018-04-27 21:56:56 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ratlog',
+    packages=['ratlog'],
+    version='',
+    description=':rat: Ratlog Python library - Application Logging for Rats, Humans and Machines',
+    author='Alex Ward',
+    author_email='alxwrd@googlemail.com',
+    url='https://github.com/alxwrd/ratlog.py',
+    long_description=open('README.md').read(),
+    license='MIT',
+)
